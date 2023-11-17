57c26b9499885c7ebd50d679ad532b31b06b8083
hua0-richard <r8hua@uwaterloo.ca>
2023-11-16 20:13:16 -0500

    Added SQLITE3 DB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
 // server/index.js
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("data.db", sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to Database");
+});
+
+// db.close((err) => {
+//   if (err) { console.err(err.message)}
+// })
 
 const PORT = process.env.PORT || 3001;
 
